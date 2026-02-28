9a3902c5aed64fb883e146e72f48bb480110602a
Daniela02 <daniela.sandu122@gmail.com>
2026-02-28 11:34:04 +0200

    Fix index and enhance tests

--- typescript/src/tests/TimeDepositCalculator.test.ts
+++ typescript/src/tests/TimeDepositCalculator.test.ts
@@ -1,11 +1,38 @@
 import { TimeDeposit } from '../TimeDeposit'
 import { TimeDepositCalculator } from '../TimeDepositCalculator'
 
-
-test('Should update balance', () => {
+test('Should update balance basic plan', () => {
   const plans: TimeDeposit[] = [new TimeDeposit(1, 'basic', 1234567.0, 45)]
   const calc = new TimeDepositCalculator()
   calc.updateBalance(plans)
 
-  expect(1).toBe(1)
+  expect(plans.length).toBe(1)
+  expect(plans[0].balance).toBe(1235595.81)
+  expect(plans[0].days).toBe(45)
+  expect(plans[0].planType).toBe('basic')
+  expect(plans[0].id).toBe(1)
+})
+
+test('Should update balance premium plan', () => {
+  const plans: TimeDeposit[] = [new TimeDeposit(1, 'premium', 1234567.0, 55)]
+  const calc = new TimeDepositCalculator()
+  calc.updateBalance(plans)
+
+  expect(plans.length).toBe(1)
+  expect(plans[0].balance).toBe(1239711.03)
+  expect(plans[0].days).toBe(55)
+  expect(plans[0].planType).toBe('premium')
+  expect(plans[0].id).toBe(1)
+})
+
+test('Should update balance student plan', () => {
+  const plans: TimeDeposit[] = [new TimeDeposit(1, 'student', 1234567.0, 45)]
+  const calc = new TimeDepositCalculator()
+  calc.updateBalance(plans)
+
+  expect(plans.length).toBe(1)
+  expect(plans[0].balance).toBe(1237653.42)
+  expect(plans[0].days).toBe(45)
+  expect(plans[0].planType).toBe('student')
+  expect(plans[0].id).toBe(1)
 })
